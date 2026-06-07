f8bf4a7174edc673423ed28e4bd12453b2e12b18
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2026-06-07 22:04:25 +0700

    feat : connect to TiDB Cloud

---
+++ lib/prisma.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+    globalForPrisma.prisma ||
+    (() => {
+        // 1. Kita bedah URL TiDB dari file .env
+        const dbUrl = new URL(process.env.DATABASE_URL as string);
+
+        // 2. Kita masukkan konfigurasinya dalam bentuk Object
+        const adapter = new PrismaMariaDb({
+            host: dbUrl.hostname,
+            port: Number(dbUrl.port) || 4000,
+            user: dbUrl.username,
+            password: dbUrl.password,
+            database: dbUrl.pathname.substring(1), // Menghapus tanda '/' di awal nama database
+            ssl: { rejectUnauthorized: true },     // KUNCI UTAMA: Memaksa enkripsi SSL menyala!
+        });
+
+        // 3. Injeksi ke Prisma Client
+        return new PrismaClient({
+            adapter,
+            log: ["query"]
+        });
+    })();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
